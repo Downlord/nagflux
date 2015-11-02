2ae8a7b708d288f7efcf071947d294b021e7b32c
Helge Laurisch <Downlord@gmail.com>
2015-11-02 01:44:45 +0100

    Update performanceData.go

--- collector/spoolfile/performanceData.go
+++ collector/spoolfile/performanceData.go
@@ -24,20 +24,11 @@ type PerformanceData struct {
 }
 
 func (p *PerformanceData) String() string {
-//	tableName := fmt.Sprintf(`%s%s%s%s%s%s%s%s%s`,
-//		p.hostname, p.fieldseperator,
-//		p.service, p.fieldseperator,
-//		p.command, p.fieldseperator,
-//		p.performanceLabel, p.fieldseperator,
-//		p.performanceType)
-	//tableName := fmt.Sprintf(`%s%s%s%s%s%s%s`, p.service, p.fieldseperator ,`command=`, p.command, p.fieldseperator , `host=` , p.hostname )
+	if p.awsinstanceid == "" {
+		p.awsinstanceid = "empty"
+	}
 	tableName := fmt.Sprintf(`%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s`, p.service, p.fieldseperator ,`command=`, p.command, p.fieldseperator ,`awsinstanceid=`, p.awsinstanceid, p.fieldseperator ,`awsservice=`, p.awsservice, p.fieldseperator ,`awsserviceid=`, p.awsserviceid, p.fieldseperator ,`awsstage=`, p.awsstage, p.fieldseperator, `host=` , p.hostname )
-	//if p.performanceType != "value" {
-	//	//tableName += fmt.Sprintf(`%s%s%s%s`, p.fieldseperator , p.performanceLabel, `=`, p.performanceType)
-	//	tableName += fmt.Sprintf(`%s%s%s`, p.fieldseperator , `perftype=`, p.performanceType)
-	//}
 	if p.unit != "" {
-//		tableName += fmt.Sprintf(`%s%s%s`, p.fieldseperator , `unit=`, p.unit)
 		p.tags["unit"] = p.unit
 	}
 
@@ -50,8 +41,6 @@ func (p *PerformanceData) String() string {
 	if len(p.mvals) > 0 {
 		tableName += fmt.Sprintf(` %s`, helper.PrintMapAsString(p.mvals, ",", "="))
 	}
-
-   //tableName += fmt.Sprintf(`,%s=%s`, p.performanceLabel, p.value)
 
    tableName += fmt.Sprintf(` %s`, p.time)
 	return tableName
